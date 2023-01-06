9d1c7f1e693020b5660f76de47dc8e65fa4c9d7a
Yoonis Ali <yoonisali24@Yooniss-Air.home>
2023-01-06 10:59:34 -0800

    add index, create, and details route for controllers

---
+++ SillyStringz/Controllers/EngineersController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SillyStringz.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SillyStringz.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly SillyStringzContext _db;
+
+    public EngineersController(SillyStringzContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Engineer thisEngineer = _db.Engineers
+                              .Include(engineer => engineer.JoinEntities)
+                              .ThenInclude(join => join.Machine)
+                              .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
+  }
+}
---
+++ SillyStringz/Controllers/MachinesController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SillyStringz.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SillyStringz.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly SillyStringzContext _db;
+
+    public MachinesController(SillyStringzContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Machines.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Machine thisMachine = _db.Machines
+                              .Include(machine => machine.JoinEntities)
+                              .ThenInclude(join => join.Engineer)
+                              .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
+
+  }
+}
